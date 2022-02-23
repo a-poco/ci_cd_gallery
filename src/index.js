281e8c765a1bd30e49c8a5bc2b7e041acb55cc01
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-02-23 11:57:13 +0100

    webpack setup 95% complete

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './styles/main.scss'
+//import example from './images/example.png'
+// Create heading node
+const heading = document.createElement('h1')
+heading.textContent = 'Interesting!'
+
+
+// Append heading node to the DOM
+// const app = document.querySelector('#root')
+// app.append(heading)
+
+// Create a class property without a constructor
+class Game {
+    name = 'Violin Charades'
+  }
+  const myGame = new Game()
+  // Create paragraph node
+  const p = document.createElement('p')
+  p.textContent = `I like ${myGame.name}.`
+
+const app = document.querySelector('#root')
+app.append(heading, p)
