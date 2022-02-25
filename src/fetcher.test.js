efd7cb97006de093050ea5eff9e49ad5b91fe8d9
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-02-25 15:59:39 +0100

    enabled SPA 2.0

--- src/fetcher.test.js
+++ src/fetcher.test.js
@@ -1,17 +1,3 @@
-const jsdom = require('jsdom');
-const { appendToElem } = require('./fetcher');
-
-const { JSDOM } = jsdom;
-
-const dom = new JSDOM('<!DOCTYPE html><html><head></head><body><div id="gallery">TESTESTTEST</div></body></html>');
-const { document } = dom.window;
-
-test.skip('should add 10 elements to div #gallery correct cc', () => {
-  appendToElem('cats');
-  const galleryLength = document.getElementsByTagName('img').length;
-  expect(galleryLength).toBe(1);
-});
-
 describe('Mock Test', () => {
   test('2=2', () => {
     expect(2).toEqual(2);
--- src/index.js
+++ src/index.js
@@ -1,41 +1,40 @@
 import './styles/main.scss';
 
 let state = {
-  images: []
+  images: [],
 };
 
-const createTemplate = state => {
-  let imagesHTMLString = ""
-  state.images.forEach(element => {
-    imagesHTMLString += `<img src=${element}></img>`
+const createTemplate = currentState => {
+  let imagesHTMLString = '';
+  currentState.images.forEach(element => {
+    imagesHTMLString += `<img src=${element}></img>`;
   });
-  return imagesHTMLString
-}
+  return imagesHTMLString;
+};
 
 const render = (htmlString, el) => {
-  el.innerHTML = htmlString;
+  const element = el;
+  element.innerHTML = htmlString;
 };
 
-const updateState = (newState) => {
-state = { ...state, ...newState }; 
-window.dispatchEvent(new Event("statechange"));
+const updateState = newState => {
+  state = { ...state, ...newState };
+  window.dispatchEvent(new Event('statechange'));
 };
 
-window.addEventListener("statechange", () => {
-  render(createTemplate(state), document.querySelector("#gallery"));
+window.addEventListener('statechange', () => {
+  render(createTemplate(state), document.querySelector('#gallery'));
 });
 
-const imageFetcher = query => {
-  return fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.KEY}&query=${query}`)
-    .then(res => res.json())
-    .then(test => {
-      const urlArray = []
-      test.results.forEach(elem => {
-        urlArray.push(elem.urls.small)
-      })
-      updateState({ images: [...urlArray] });
-    })
-};
+const imageFetcher = query => fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.KEY}&query=${query}`)
+  .then(res => res.json())
+  .then(test => {
+    const urlArray = [];
+    test.results.forEach(elem => {
+      urlArray.push(elem.urls.small);
+    });
+    updateState({ images: [...urlArray] });
+  });
 
 document.getElementById('searchButton').addEventListener('click', () => {
   document.querySelector('#gallery').innerHTML = '';
